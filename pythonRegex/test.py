081290d251ea7f427bc0bed9279bcb01815b545e
lingxiao <13656261355@163.com>
2024-06-20 15:26:54 +0800

    ddd

--- pythonRegex/test.py
+++ pythonRegex/test.py
@@ -1,19 +1,16 @@
 import re
 
-pattern = r'.+'
-# pattern = r'(\d+)([a-z]+)'
+# 横向匹配
+def hor_match01():
+    str = "abc abbc abbbc abbbbc abbbbbc abbbbbbc"
 
-string = '123abc645'
+    pattern = r'ab{2,5}c'
 
-match = re.match(pattern, string)
-# if match:
-#     print('Match found:', match.group())
-# else:
-#     print('No match')
+    matchs = re.findall(pattern, str)
+    if matchs:
+        print('Match found:', matchs)
+        # Match found: ['abbc', 'abbbc', 'abbbbc', 'abbbbbc']
+    else:
+        print('No match')
 
-if match:
-    print('Full match:', match.group()) 
-    # print('Full match:', match.group(0))   # 整个匹配到的字符串
-    # print('First group:', match.group(1))  # 第一个组（\d+）
-    # print('Second group:', match.group(2)) # 第二个组（[a-z]+）
-    # print('Second group:', match.group(3)) # 第三个组（\d+）
+hor_match01()
